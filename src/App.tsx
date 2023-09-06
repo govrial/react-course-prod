a3177fefd9ba86f5151c80b762d8717ff39d9ae8
govrial <36007441+govrial@users.noreply.github.com>
2023-09-07 02:41:19 +0300

    #add #app - add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './index.scss';
-import Counter from "./components/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
+import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
+import MainPageAsync from "./pages/MainPage/MainPage.async";
+
 const App = () => {
     return (
         <div className={'app'}>
-            app
-            <Counter />
+            <Link to={'/'}>MAIN</Link>
+            <Link to={'/about'}>ABOUT</Link>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
