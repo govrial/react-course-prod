475f54d924a503a5e31bc2e56b4d649a524bd9fc
govrial <36007441+govrial@users.noreply.github.com>
2023-09-07 03:13:45 +0300

    #add #styles - add styles and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, {Suspense} from 'react';
-import './index.scss';
+import React, {Suspense, useState} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
 import MainPageAsync from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {toggleTheme, theme} = useTheme()
+
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>MAIN</Link>
             <Link to={'/about'}>ABOUT</Link>
             <Suspense fallback={<div>...Loading</div>}>
