20d1a8f08890eaa9ff412c8f18b0673e1a7243ce
govrial <36007441+govrial@users.noreply.github.com>
2023-09-13 00:50:29 +0300

    #add #webpack #plugin - add hot module plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,6 +17,7 @@ export default function buildPlugins({paths, isDev}: BuildOptions): webpack.Webp
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
